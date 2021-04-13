a9cb8ea7d42d4534a7f44d6553580600dfdcf2c8
RossieK <rosi.kirova96@gmail.com>
2021-04-13 18:28:06 +0300

    Customize inputs

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ function randomColors() {
 
         //Check for contrast
         checkTextContrast(randomColor, hexText);
+
+        //Initial colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     })
 }
 
@@ -35,6 +44,21 @@ function checkTextContrast(color, text) {
     } else {
         text.style.color = "white";
     }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    //Scale saturation
+    const noSaturation = color.set('hsl.s', 0);
+    const fullSaturation = color.set('hsl.s', 1);
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+    //Scale brightness
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBrightness = chroma.scale(['black', midBright, 'white']);
+
+    //Update input colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204,204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`
 }
 
 randomColors();
