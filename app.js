241d58d41325a1601e95c43af56b3f9d51a990d8
RossieK <rosi.kirova96@gmail.com>
2021-04-14 17:21:53 +0300

    Add copy to clipboard functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
+const popup = document.querySelector('.copy-container');
 let initialColors;
 
 //Event listeners
@@ -14,6 +15,18 @@ colorDivs.forEach((div, index) => {
     div.addEventListener('change', () => {
         updateTextUI(index);
     });
+});
+
+currentHexes.forEach(hex => {
+    hex.addEventListener('click', () => {
+        copyToClipboard(hex);
+    });
+});
+
+popup.addEventListener('transitionend', () => {
+    const popupBox = popup.children[0];
+    popup.classList.remove('active');
+    popupBox.classList.remove('active');
 });
 
 //Functions
@@ -133,6 +146,20 @@ function resetInputs() {
             slider.value = Math.floor(saturationValue * 100) / 100;
         }
     })
+}
+
+function copyToClipboard(hex) {
+    const element = document.createElement('textarea');
+    element.value = hex.innerText;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+
+    //Pop up animation
+    const popupBox = popup.children[0];
+    popup.classList.add('active');
+    popupBox.classList.add('active');
 }
 
 randomColors();
